de55e3d99b1ec02a7aa9c1db1416723e31fc47e7
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2025-09-07 19:24:50 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -15,19 +15,22 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://bookmanager-app.onrender.com"
 ];
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true, // allow cookies
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use(cors({}));
-
-// ✅ This makes Express reply to preflight OPTIONS
-// app.options("*", cors());
+app.options("*", cors()); // preflight support
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
