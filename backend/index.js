3c2d5ce068e204c174aa9086d4e66aca20f3e6f6
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2025-09-07 19:51:47 +0530

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -15,22 +15,27 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://bookmanager-app.onrender.com"
 ];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
-app.options("*", cors()); // preflight support
+app.use(cors({
+  origin: 'https://bookmanager-app.onrender.com',
+  credentials: true
+}));
+
+// app.options("*", cors()); // preflight support
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
